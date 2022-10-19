7fa8d80bfcba15c6710b19b84ac5c49c64f65539
jeganvicent <jvincent@boodskap.io>
2022-10-19 19:23:23 +0530

    Added version processing logic

--- build.sh
+++ build.sh
@@ -4,4 +4,7 @@ npm install
 
 node build.js
 
-docker build -t boodskapiot/ui:5.0.0-alpha . -f Dockerfile
+VERSION=$(cat package.json | /usr/bin/jq -r '.version')
+
+docker build -t boodskapiot/ui:${VERSION} . -f Dockerfile
+
