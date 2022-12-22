6b27a57191a347b3548d18df2a35ac01dda09a3d
DhanaVigneshT <110087004+DhanaVigneshT@users.noreply.github.com>
2022-12-22 10:01:34 +0530

    update code

--- webapps/controllers/statistics-dashboard.js
+++ webapps/controllers/statistics-dashboard.js
@@ -180,10 +180,8 @@ function date() {
         ranges: {
             'Today': [moment(), moment()],
             'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-            'Last 7 Days': [moment().subtract(6, 'days'), moment()],
             'Last 30 Days': [moment().subtract(29, 'days'), moment()],
             'This Month': [moment().startOf('month'), moment().endOf('month')],
-            'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')],
             'Last 3 Months': [moment().subtract(3, 'month'), moment().subtract(1, 'month')],
             'Last 6 Months': [moment().subtract(6, 'month'), moment()]
         }
