43cb5c49617e46f5a53ab9a7bbc97cf89c27b1cd
Arul Prakash <arulprakashv@boodskap.io>
2023-01-09 10:52:53 +0530

    file edits common and domain page

--- webapps/controllers/domain.js
+++ webapps/controllers/domain.js
@@ -1,6 +1,6 @@
 var logoPathId = '';
 var selectedId = null;
-var themeProp = {};
+
 $(document).ready(function () {
    
 
@@ -103,92 +103,6 @@ function getDarkSkyApiKey() {
         } else {
 
         }
-        $("#domainModal").modal('show');
-
-    })
-}
-
-function getDomainTheme() {
-    getDomainProperty(DOMAIN_THEME_PROPERTY, function (status, data) {
-        if (status) {
-
-            themeProp = JSON.parse(data.value);
-            header 
- $("#header").css('background-color', themeProp.headerBg)
- 
- 
-            $(".headerBg").css('background-color', themeProp.headerBg)
-            $(".subHeaderBg").css('background-color', themeProp.subHeaderBg)
-            $(".panelHeaderBg").css('background-color', themeProp.panelHeaderBg)
-            $(".bodyBg").css('background-color', themeProp.bodyBg)
-
-            $(".headerBg").css('color', themeProp.textColor)
-            $(".user_profile_name").css('color', themeProp.textColor)
-            $(".subHeaderBg").css('color', themeProp.textColor)
-
-        }
-
-        $("#bodyLayout").val(themeProp.layout ? themeProp.layout : DEFAULT_THEME.layout);
-
-
-        if ($("#bodyLayout").val() === 'container') {
-            $(".divPanel").css('width', '50%')
-        } else {
-            $(".divPanel").css('width', '100%')
-        }
-
-        $('#headerBg').colorpicker({
-            color: themeProp.headerBg ? themeProp.headerBg : DEFAULT_THEME.headerBg,
-            format: 'hex'
-        });
-        $('#subHeaderBg').colorpicker({
-            color: themeProp.subHeaderBg ? themeProp.subHeaderBg : DEFAULT_THEME.subHeaderBg,
-            format: 'hex'
-        });
-        $('#panelHeaderBg').colorpicker({
-            color: themeProp.panelHeaderBg ? themeProp.panelHeaderBg : DEFAULT_THEME.panelHeaderBg,
-            format: 'hex'
-        });
-        $('#textColor').colorpicker({
-            color: themeProp.textColor ? themeProp.textColor : DEFAULT_THEME.textColor,
-            format: 'hex'
-        });
-        $('#bodyBg').colorpicker({
-            color: themeProp.bodyBg ? themeProp.bodyBg : DEFAULT_THEME.bodyBg,
-            format: 'hex'
-        });
-
-        /*$("#headerBg").spectrum({
-            showPalette: true,
-            showInput: true,
-            showInitial: true
-        });
-        $("#headerBg").spectrum("set", themeProp.headerBg ? themeProp.headerBg : DEFAULT_THEME.headerBg);
-        $("#subHeaderBg").spectrum({
-            showPalette: true,
-            showInput: true,
-            showInitial: true
-        });
-        $("#subHeaderBg").spectrum("set", themeProp.subHeaderBg ? themeProp.subHeaderBg : DEFAULT_THEME.subHeaderBg);
-        $("#panelHeaderBg").spectrum({
-            showPalette: true,
-            showInput: true,
-            showInitial: true
-        });
-        $("#panelHeaderBg").spectrum("set", themeProp.panelHeaderBg ? themeProp.panelHeaderBg : DEFAULT_THEME.panelHeaderBg);
-        $("#textColor").spectrum({
-            showPalette: true,
-            showInput: true,
-            showInitial: true
-        });
-        $("#textColor").spectrum("set", themeProp.textColor ? themeProp.textColor : DEFAULT_THEME.textColor);
-
-        $("#bodyBg").spectrum({
-            showPalette: true,
-            showInput: true,
-            showInitial: true
-        });
-        $("#bodyBg").spectrum("set", themeProp.bodyBg ? themeProp.bodyBg : DEFAULT_THEME.bodyBg);*/
         $("#domainModal").modal('show');
 
     })
