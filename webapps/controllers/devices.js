00f4f5863740dd1eaae01fa03e8209234d491ba8
ajpkponnarasi <ponnarasia@boodskap.io>
2023-01-30 15:40:41 +0530

    device models changes

--- webapps/controllers/devices.js
+++ webapps/controllers/devices.js
@@ -13,8 +13,6 @@ $(document).ready(function () {
 
 function loadDeviceList() {
 
-    const self = this;
-
 
     if (deviceTable) {
         deviceTable.destroy();
@@ -137,7 +135,7 @@ function loadDeviceList() {
                 }
             },
             aoColumns: fields,
-            // "bProcessing": true,
+            "bProcessing": true,
             "bServerSide": true,
             "sAjaxSource": API_BASE_PATH + '/elastic/search/query/' + API_TOKEN_ALT,
             "fnServerData": function (sSource, aoData, fnCallback, oSettings) {
