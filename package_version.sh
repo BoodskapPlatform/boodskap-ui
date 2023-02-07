539dc4ee542803115291798f5d7596cca15c21a4
subosand <subosand@boodskap.io>
2023-02-07 15:57:24 +0530

    changes in package_version file

--- package_version.sh
+++ package_version.sh
@@ -4,5 +4,5 @@ npm version patch
 git add --all
 git commit -m "new version updated"
 # git commit -m "${(cat package.json | /usr/bin/jq -r '.version')}"
-git push -o ci.skip
+git push --push-option="ci.skip"
 
