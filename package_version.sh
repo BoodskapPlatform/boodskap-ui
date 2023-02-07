429300e5107e9c754e3741a6430d6c652788c257
subosand <subosand@boodskap.io>
2023-02-07 13:48:21 +0530

    pakcage_version updated

--- package_version.sh
+++ package_version.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 npm version patch 
+git add --all
+git commit -m "new version updated"
+# git commit -m "${(cat package.json | /usr/bin/jq -r '.version')}"
+git push -o ci.skip
 
-# git add --all
-# git commit -m "new version updated"
-# # git commit -m "${(cat package.json | /usr/bin/jq -r '.version')}"
-# git push common v5.0
