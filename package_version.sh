9f5cd8556c5dd5f3353005a2f8554f9e01599019
subosand <subosand@boodskap.io>
2023-02-09 09:47:37 +0530

    changes in package_version.sh

--- package_version.sh
+++ package_version.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-npm version patch 
+npm version patch -m "Bump version to %s [skip ci]" --force 
 git add --all
-git commit -a -m "[skip ci] Version updated new version updated"
+git commit -m Version updated new version updated
 # git commit -m "${(cat package.json | /usr/bin/jq -r '.version')}"
 git push common v5.0
 
