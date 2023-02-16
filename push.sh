e6f28cad0bf708c5d84ef6d87d87aadcd39ba655
subosand <subosand@boodskap.io>
2023-02-16 16:04:11 +0530

    changes in push.sh file

--- push.sh
+++ push.sh
@@ -5,3 +5,6 @@ VERSION=$(cat package.json | /usr/bin/jq -r '.version')
 docker push boodskapiot/ui:${VERSION} 
 
 echo "Docker pushed successfully boodskapiot/ui:${VERSION}"
+git add .
+git commit -m "version update"
+git push --no-verify
