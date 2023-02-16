840964507bc8a52a99a726fc71379e6bf11e1fe0
subosand <subosand@boodskap.io>
2023-02-16 10:41:58 +0530

    changes in devops file for autoamtion

--- build.sh
+++ build.sh
@@ -1,7 +1,33 @@
 #!/bin/bash
+# Parse current version number
+JSON_FILE="/home/ubuntu/build/v5-boodskap-ui/test/boodskap-ui/package.json"
+version=$(cat $HOME/build/version.txt)
+# Split version number into major, minor, and patch components
+IFS='.' read -r -a version_components <<< "$version"
+major=${version_components[0]}
+minor=${version_components[1]}
+patch=${version_components[2]}
 
-npm install
+# Determine which component to increment (default to patch)
+component=${1:-patch}
+
+if [ "$minor" -ge 10 ] && [ "$patch" -ge 30 ]; then
+  major=$((major+1))
+  minor=0
+  patch=0
+elif [ "$patch" -ge 30 ]; then
+  minor=$((minor+1))
+  patch=0
+else
+  patch=$((patch+1))
+fi
+
+# Update version number in version.txt
+echo "$major.$minor.$patch" > $HOME/version.txt
+jq ".version = \"$major.$minor.$patch\"" "$JSON_FILE" > tmp.json
+mv tmp.json "$JSON_FILE"
+lversion="$major.$minor.$patch"
+# Print new version number
 node build.js
-VERSION=$(cat package.json | /usr/bin/jq -r '.version')
-docker build -t boodskapiot/ui:${VERSION} . -f Dockerfile
+docker build -t boodskapiot/ui:${lversion} . -f Dockerfile
 
